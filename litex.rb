334297256196fcd871c3204e607200a9efe82603
kidhero <scanfandprintf@icloud.com>
2025-08-01 14:59:14 +0800

    Fix download URL for Litex to point to the correct version 0.1.7-beta

--- litex.rb
+++ litex.rb
@@ -1,7 +1,7 @@
 class Litex < Formula
   desc "Litex: an intuitive and scalable formal language"
   homepage "https://litexlang.org"
-  url "https://github.com/litexlang/golitex/releases/download/latest/litex_0.1.7-beta_darwin_amd64"
+  url "https://github.com/litexlang/golitex/releases/download/v0.1.7-beta/litex_0.1.7-beta_darwin_amd64"
   sha256 "e8247d0e8a11bb11cd6bdde033df15546215a3967aaaa7b180cbac194f365fbe"
   license "Apache-2.0"
   version "0.1.7-beta"
