1caa7eb4a07454ffa298ef0fb39877227c8ffbbb
kidhero <scanfandprintf@icloud.com>
2025-08-11 11:10:29 +0800

    Update litex.rb to version 0.1.9-beta with new download URL and SHA256 checksum

--- litex.rb
+++ litex.rb
@@ -1,12 +1,12 @@
 class Litex < Formula
   desc "Litex: an intuitive and scalable formal language"
   homepage "https://litexlang.org"
-  url "https://github.com/litexlang/golitex/releases/download/0.1.8-beta/litex_0.1.8-beta_darwin_amd64"
-  sha256 "5a0bf41b8a76667bce7ecf2c8d366180757094207cabd7776028ad453d284b46"
+  url "https://github.com/litexlang/golitex/releases/download/0.1.9-beta/litex_0.1.9-beta_darwin_amd64"
+  sha256 "96c33991b7a0f590a6a66760cb42df4a708c7fde9d7e4d7fdad77a4e9bea962a"
   license "Apache-2.0"
-  version "0.1.8-beta"
+  version "0.1.9-beta"
 
   def install
-    bin.install "litex_0.1.8-beta_darwin_amd64" => "litex"
+    bin.install "litex_0.1.9-beta_darwin_amd64" => "litex"
   end
 end
