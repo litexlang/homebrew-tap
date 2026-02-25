ca2cfee1914defe0cbaba0174a059a5b0ad82716
kidherofun <scanfandprintf@icloud.com>
2026-02-25 06:47:00 +0000

    update version to 0.8.01-beta

--- litex.rb
+++ litex.rb
@@ -1,12 +1,12 @@
 class Litex < Formula
   desc "Litex: an intuitive and scalable formal language"
   homepage "https://litexlang.org"
-  url "https://github.com/litexlang/golitex/releases/download/0.7.11-beta/litex_0.7.11-beta_darwin_amd64"
-  sha256 "ee4f075e54bad10fa372e501961bedfb23dfd19f40fad50bff2a0f195b0d6ede"
+  url "https://github.com/litexlang/golitex/releases/download/0.8.01-beta/litex_0.8.01-beta_darwin_amd64"
+  sha256 "5ae95365b686324a908dc9a630ee0e5b8c32bb1e8c18371ac32641124430656b"
   license "Apache-2.0"
-  version "0.7.11-beta"
+  version "0.8.01-beta"
 
   def install
-    bin.install "litex_0.7.11-beta_darwin_amd64" => "litex"
+    bin.install "litex_0.8.01-beta_darwin_amd64" => "litex"
   end
 end
